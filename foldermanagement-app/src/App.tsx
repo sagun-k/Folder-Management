32fa65e54665d1c14bfe26ab913ba44f6a287ba2
sagunstart <sagun.startgoc@gmail.com>
2022-03-21 17:48:42 +0545

    Some changes added

--- foldermanagement-app/src/App.tsx
+++ foldermanagement-app/src/App.tsx
@@ -3,11 +3,41 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [mainFolder, setMainFolder] = useState<any>([])
+  const [name, setName] = useState('')
+  const addFolder: any = (newFolder: any, e: any) => {
+    newFolder = {
+      name: name,
+      id: Math.random(),
+      file: [],
+    }
+    setMainFolder((state: any) => [...state, newFolder])
+    setName('')
+  }
+  const addEachFile: any = (e: any) => {
+    console.log(e)
+  }
   return (
     <div className="App">
-     
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => {
+          e.preventDefault()
+          setName(e.target.value)
+        }}
+      />
+      <button onClick={addFolder}> Add folder</button>
+      <div style={{ display: 'flex', gap: '2rem', overflow: 'scroll' }}>
+        {mainFolder.length > 0 &&
+          mainFolder.map((item: any, index: any) => {
+            return (
+              <button key={index} onClick={() => addEachFile(item.id)}>
+                <h1 style={{ color: 'blue' }}>{item.name}</h1>
+              </button>
+            )
+          })}
+      </div>
     </div>
   )
 }
